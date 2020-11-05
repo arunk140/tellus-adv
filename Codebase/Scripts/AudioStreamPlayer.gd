764060c27c9d72eaf1dd524be670287ff914d70c
Arun Khanchandani <arunk140@gmail.com>
2020-11-06 07:55:21 +1300

    Added sound effect for starting/pausing game

--- Codebase/Scripts/AudioStreamPlayer.gd
+++ Codebase/Scripts/AudioStreamPlayer.gd
@@ -7,27 +7,30 @@ export var transition_type = 1 # TRANS_SINE
 const MAX_VOL = -20
 const MIN_VOL = -60
 
-onready var audiostream = $AudioStreamPlayer
+onready var audiostream = $BGMusic
 
 var is_paused = false
 
 func _ready():
+	$PauseSound.play()
 	fade_in()
 
 func _input(event):
 	if Input.is_action_just_pressed("ui_cancel"):
 		if is_paused:
+			$PauseSound.play()
 			fade_in()
 		else:
+			$PauseSound.play()
 			fade_out()
 		is_paused = !is_paused
 
 func fade_out():
-	$Tween.interpolate_property($AudioStreamPlayer, "volume_db", MAX_VOL, MIN_VOL, fade_out_duration, transition_type, Tween.EASE_IN, 0)
+	$Tween.interpolate_property($BGMusic, "volume_db", MAX_VOL, MIN_VOL, fade_out_duration, transition_type, Tween.EASE_IN, 0)
 	$Tween.start()
 		
 func fade_in():
-	$Tween.interpolate_property($AudioStreamPlayer, "volume_db", MIN_VOL, MAX_VOL, fade_in_duration, transition_type, Tween.EASE_IN, 0)
+	$Tween.interpolate_property($BGMusic, "volume_db", MIN_VOL, MAX_VOL, fade_in_duration, transition_type, Tween.EASE_IN, 0)
 	$Tween.start()
 
 
--- Codebase/Scripts/TitleScreen.gd
+++ Codebase/Scripts/TitleScreen.gd
@@ -6,4 +6,3 @@ func _input(event):
 
 func _on_Button_pressed():
 	get_tree().change_scene("res://Scenes/BaseWorld.tscn")
-	
